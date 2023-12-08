175f1cf52dd99cf9e3dac02ae9eeb15e6cd633b3
YQYjin <yqyjinhuang@163.com>
2023-12-08 22:00:37 +0800

    创建User子模块,成功实现三个模块的启动

---
+++ SpringCloud_User/src/main/java/com/example/springcloud_user/SpringCloudUserApplication.java
@@ -0,0 +1,24 @@
+package com.example.springcloud_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudUserApplication {
+
+    @Bean
+    @LoadBalanced
+    // 负载均衡功能
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudUserApplication.class, args);
+    }
+
+}
